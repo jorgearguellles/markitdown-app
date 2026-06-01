72ad8b158faec625f425d233cd0d28b416d9b335
Jorge Argüelles <jorgeariasarguelles@gmail.com>
2026-06-01 03:37:57 -0500

    Install speed-insights by Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-mono">{children}</body>
+      <body className="font-mono">{children} <SpeedInsights /> </body>
     </html>
   );
 }
